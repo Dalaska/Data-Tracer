3d17dc70a397b05bcb1cda9a613fd91882c7af8c
Darui Zhang <neozdr@gmail.com>
2020-03-07 17:16:28 +0800

    readme

--- trace_points.m
+++ trace_points.m
@@ -1,53 +1,22 @@
-% trace image from point
-src = imread('to_trace.png');
+% extract data from image trace image from point
+
+% load image 
+src = imread('car_sales.png');
 
 imshow(src)
 
 hold on;
 
-p=100; q=100; r=100; 
+p=100; q=100;
 
-
+% get points
 [Px,Py] = ginput(p); plot(Px,Py,'o')
 
+% get origin
 [Qx,Qy] = ginput(q); plot(Qx,Qy,'x')
 
-[Rx,Ry] = ginput(r); plot(Rx,Ry,'*')
+
+% save to file
+%save('trace_data') 
 
 
-save('trace_data') 
-%%========== extra =======
-
-load('trace_data') 
-figure(1)
-%imshow(src)
-hold on
-Px(end) = max(Qx);
-plot(Px-min(Px),-(Py-Ry));
-plot(Qx-min(Qx),-(Qy-Ry));
-
-x = Px-min(Px);
-v = -(Py-Ry);
-xq = linspace(0,max(x),20);
-vq1 = interp1(x,v,xq);
-
-x2 = Qx-min(Qx);
-v2 = -(Qy-Ry);
-xq2 = linspace(0,max(x2),20);
-vq2 = interp1(x2,v2,xq2);
-
-figure(2)
-hold on
-plot(xq,vq1);
-plot(xq2,vq2);
-
-ratio = vq1./vq2;
-yq = smooth(ratio)
-figure(3)
-hold on
-plot(xq,ratio);
-plot(xq,yq)
-
-figure(4)
-plot(xq*4/601,yq,'-ok')
-axis([0 4 0 3.5])
